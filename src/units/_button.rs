08a1e195ef882e0894ac878e58aae54140b6cbe6
karsh <utkarsh@cyberfear.com>
2024-01-25 13:43:15 +1100

    📐 testing design structures
    to maximise code reuse, I am trying to implement common methods of
    initialising, updating and blinking different units in within the trait
    declaration. with this, new units wouldn't have to reuse the same code,
    since most of these functionalities would remain same.
    
    current challenges-
    - problems with defining actions with mutable self
    - problems with defining traits altogether to fit the need
    
    accomplishments-
    - created a default trait with variable length for different units
    - have a somewhat consistent syntax right now to allow trait impl

--- src/units/_button.rs
+++ src/units/_button.rs
@@ -1,47 +1,12 @@
-use gpiod;
-use super::unit;
+// units/_buttons.rs
+
+use super::input::{Structure, Interface};
 use crate::config::details::{BUTTON_PINS, BUTTON_COUNT};
 
-const PIN_COUNT: usize = 5;
+pub type Button = Structure<BUTTON_COUNT>;
 
-pub struct Button {
-    line: gpiod::Lines<gpiod::Input>,
-    sleep: [u32; PIN_COUNT],
-    pub call: u16,
-}
-
-impl unit::Input for Button {
-    // Initialize and return a vector of buttons
-    fn init(chip: &gpiod::Chip) -> Button {
-        let pins: [u32; PIN_COUNT] = [
-            25, // 0  terminate the program
-            17, // 1  ports forwarded for virtual buttons
-            27, // 2  ports forwarded for virtual buttons
-            22, // 3  ports forwarded for virtual buttons
-            // ...add new buttons here
-            24  // 4  toggle shift mode
-        ];
-
-        let sleep_status: [u32; PIN_COUNT] = Default::default();
-
-        let options = gpiod::Options::input(pins)
-            .consumer("buttons");
-
-        let connection_line = chip
-            .request_lines(options)
-            .expect("failed");
-
-        return Button {
-            line: connection_line, 
-            sleep: sleep_status,
-            call: 0,
-        };
-    }
-
-    fn update(&mut self, counter: &u32) {
-        if 0 == self.sleep[0] {
-            println!("{counter}");
-        }
-    }
+impl Interface<BUTTON_COUNT> for Button {
+    const PINS: [u8; BUTTON_COUNT] = BUTTON_PINS;
+    const ID: &'static str = "Buttons";
 }
 
--- src/units/_buzzer.rs
+++ src/units/_buzzer.rs
@@ -1,35 +1,12 @@
-use gpiod;
-use super::unit;
+// units/_buzzer.rs
+
+pub use super::output::{Structure, Interface};
 use crate::config::details::{BUZZER_PINS, BUZZER_COUNT};
 
-const PIN_COUNT: usize = 1;
+pub type Buzzer = Structure<BUZZER_COUNT>;
 
-pub struct Buzzer {
-    line: gpiod::Lines<gpiod::Output>,
-    sleep: [u32; PIN_COUNT],
-}
-
-    // Initialize and return a vector of buzzers
-impl unit::Output for Buzzer {
-    fn init(chip: &gpiod::Chip) -> Buzzer {
-        let pins: [u32; PIN_COUNT] = [
-            13  // one pin
-        ];
-
-        let sleep_status: [u32; PIN_COUNT] = Default::default();
-
-        let options = gpiod::Options::output(pins)
-            .consumer("Buzzers");
-
-        let connection_line = chip
-            .request_lines(options)
-            .expect("failed to Initialize buzzers");
-
-        return Buzzer { line: connection_line, sleep: sleep_status };
-    }
-
-    fn update(&mut self, counter: &u32) {
-        // Update logic for buzzer
-    }
+impl Interface<BUZZER_COUNT> for Buzzer {
+    const PINS: [u8; BUZZER_COUNT] = BUZZER_PINS;
+    const ID: &'static str = "Buzzers";
 }
 
