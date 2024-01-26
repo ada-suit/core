0edf00ae19b5b232d9a60e70f17e734f73e77a27
karsh <utkarsh@cyberfear.com>
2024-01-26 18:11:19 +1100

    📐 somewhat working
    got somewhat working trait defined with default init procedure. still
    got some error to fix with the use of `Self` for defining `options`.
    
    key to fixing it also helps resolve more complex issues with output.rs
    that is more complicated with default implementations for all three
    functions. currently designing new solutions to find a solution.

--- src/units/input.rs
+++ src/units/input.rs
@@ -1,2 +1,39 @@
 // src/units/input.rs
 
+use gpiod;
+use crate::counter;
+use super::pulse::{Pulse, pace_value};
+
+pub struct Structure<const COUNT: usize> {
+    line:  gpiod::Lines<gpiod::Input>,
+    sleep: [u32;COUNT],
+    pub call: u16,
+}
+
+pub trait Interface<const COUNT: usize> {
+    const PINS: [u8; COUNT];
+    const ID: &'static str;
+
+    // Initialize and return a vector of buttons
+    fn init(chip: &gpiod::Chip) -> Structure<COUNT> {
+        let sleep_status: [u32; COUNT] = [0; COUNT];
+
+        let options = gpiod::Options::input(Self::PINS)
+            .consumer("{Self::ID}");
+
+        let connection_line = chip
+            .request_lines(options)
+            .expect("failed");
+
+        return Structure {
+            line: connection_line, 
+            sleep: sleep_status,
+            call: 0,
+        };
+    }
+
+    fn update(&mut self, counter: &u32) {
+        unimplemented!();
+    }
+}
+
