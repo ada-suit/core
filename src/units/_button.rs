3a0e64e538ae219a8b5ed6d38161525533b712fb
karsh <utkarsh@cyberfear.com>
2024-01-28 21:40:20 +1100

    ✨ common input init
    implemented a default way to initialise all different input units that could be connected to the core.
    
    with the current structure, the init() function could easily be overwritten

--- src/units/_button.rs
+++ src/units/_button.rs
@@ -1,12 +1,16 @@
 // units/_buttons.rs
 
-use super::input::{Structure, Interface};
+use super::input::*;
 use crate::config::details::{BUTTON_PINS, BUTTON_COUNT};
 
-pub type Button = Structure<BUTTON_COUNT>;
+pub type Button = Input<BUTTON_COUNT>;
 
-impl Interface<BUTTON_COUNT> for Button {
+impl InBase<BUTTON_COUNT> for Button {
     const PINS: [u32; BUTTON_COUNT] = BUTTON_PINS;
     const ID: &'static str = "Buttons";
+
+    fn update(&mut self, counter: &u32) {
+        std_update(self, counter);
+    }
 }
 
--- src/units/input.rs
+++ src/units/input.rs
@@ -1,7 +1,6 @@
 // src/units/input.rs
 
 use gpiod;
-use crate::counter;
 use super::pulse::{Pulse, pace_value};
 
 pub struct Structure<const COUNT: usize> {
@@ -10,12 +9,12 @@ pub struct Structure<const COUNT: usize> {
     pub call: u16,
 }
 
-pub trait Interface<const COUNT: usize> {
+pub trait InBase<const COUNT: usize>{
+    const PINS: [u32; COUNT];
     const ID: &'static str;
-    const PINS: [u32; COUNT] = [0; COUNT];
 
     // Initialize and return a vector of buttons
-    fn init(chip: &gpiod::Chip) -> Structure<COUNT> {
+    fn init(chip: &gpiod::Chip) -> Input<COUNT> {
         let sleep_status: [u32; COUNT] = [0; COUNT];
 
         let options = gpiod::Options::input(Self::PINS)
@@ -25,15 +24,17 @@ pub trait Interface<const COUNT: usize> {
             .request_lines(options)
             .expect("failed");
 
-        return Structure {
+        return Input {
             line: connection_line, 
             sleep: sleep_status,
             call: 0,
         };
     }
 
-    fn update(&mut self, counter: &u32) {
-        unimplemented!();
-    }
+    fn update(&mut self, counter: &u32);
+}
+
+pub fn std_update<const COUNT: usize>(unit: &mut Input<COUNT>, counter: &u32) {
+    unimplemented!();
 }
 
