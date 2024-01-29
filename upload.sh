07c882ba38aa48d595134b454cc3f827eadc4152
karsh <utkarsh@cyberfear.com>
2024-01-29 15:50:39 +1100

    🚚 scope change
    this repo only consists the core module of the ada project

--- upload.sh
+++ upload.sh
@@ -4,16 +4,16 @@ clear;
 
 # copy all source code to the target machine
 echo -e "\e[1;33mUpload\n===============\e[1;31m"
-scp -r ./src/* core3b+:$HOME/ada/src/.
+scp -r ./src/* core3b+:$HOME/core/src/.
 
 if [ $# -eq 0 ]; then
     # just compile the code
     echo -e "\e[1;33m\nCompile\n===============\e[1;36m"
-    ssh core3b+ 'cd ~/ada ; cargo build'
+    ssh core3b+ 'cd ~/core ; cargo build'
 
 elif [ "$1" == "run" ]; then
     # compile and run 
     echo -e "\e[1;33m\nCompile and Run\n===============\e[1;36m"
-    ssh core3b+ 'cd ~/ada ; cargo run'
+    ssh core3b+ 'cd ~/core ; cargo run'
 fi
 
