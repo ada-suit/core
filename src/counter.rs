eb9e2db91474a2807781db42255e00e2c44f9440
karsh <utkarsh@cyberfear.com>
2024-02-01 23:19:13 +1100

    🐛 fix logic error
    should check for not equal, `!=`, and not is equal `==`

--- src/counter.rs
+++ src/counter.rs
@@ -7,7 +7,7 @@
 // increment counter 
 // either increment its value by 1 or reset it back to 1
 pub fn update(counter: &mut u32)  {
-    *counter = 1 + (*counter * (*counter == u32::MAX) as u32);
+    *counter = 1 + (*counter * (*counter != u32::MAX) as u32);
     /*
      * The counter is intentionally designed to never reach 0. This ensures
      * that units and processes can halt without transitioning to the state 0.
